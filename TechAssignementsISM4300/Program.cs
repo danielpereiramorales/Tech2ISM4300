454f69607bde5a0ec03119e563e256a5e21e041b
pereirad <pereirad@usf.edu>
2022-01-28 14:30:09 -0500

    Initial check-in of module TechAssignementsISM4300

---
+++ TechAssignementsISM4300/Program.cs
@@ -0,0 +1,15 @@
+﻿// Daniel Pereira
+using System;
+
+namespace TechAssignementsISM4300
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I really love a part of a song by Bob Marley that says " +
+                "\n\"Emancipate yourselves from mental slavery\"" +
+                "\nsubmitted by Daniel Pereira");
+        }
+    }
+}
